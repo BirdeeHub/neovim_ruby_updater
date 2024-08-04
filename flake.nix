e9436a4d4936ee39827947af3d4920933f0738ab
BirdeeHub <robindbor@gmail.com>
2024-08-04 10:07:54 -0700

    formatting

--- flake.nix
+++ flake.nix
@@ -4,15 +4,16 @@
   };
   outputs = { nixpkgs, ... }: {
     packages = nixpkgs.lib.genAttrs nixpkgs.lib.platforms.all (system: let
-      pkgs = import nixpkgs { inherit system; };
-      rubyEnv = (pkgs.ruby.withPackages (p: [ p.msgpack p.multi_json ]));
-    in {
-      default = pkgs.writeShellScriptBin "nvim_ruby_updater" ''
+        pkgs = import nixpkgs { inherit system; };
+        rubyEnv = pkgs.ruby.withPackages (p: [ p.msgpack p.multi_json ]);
+      in {
+        default = pkgs.writeShellScriptBin "nvim_ruby_updater" ''
           echo "Running 'bundle update'..."
           ${rubyEnv}/bin/bundle update
           echo "Running 'bundix'..."
           ${pkgs.bundix}/bin/bundix
         '';
-    });
+      }
+    );
   };
 }
