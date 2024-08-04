3a7568c05221b9e971f84fd15647061c8021f0cf
BirdeeHub <robindbor@gmail.com>
2024-08-04 08:03:27 -0700

    first commit

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  outputs = {nixpkgs, ... }@inputs: {
+    packages = nixpkgs.lib.genAttrs nixpkgs.lib.platforms.all (system: let
+      pkgs = import nixpkgs { inherit system; };
+      rubyEnv = (pkgs.ruby.withPackages (p: [ p.msgpack ]));
+    in {
+      default = pkgs.writeShellScriptBin "nvim_ruby_updater" ''
+          echo "Running 'bundle update'..."
+          ${rubyEnv}/bin/bundle update
+          echo "Running 'bundix'..."
+          ${pkgs.bundix}/bin/bundix
+        '';
+    });
+  };
+}
